762c441b3800cd0267cd69ba161459a2c0265502
rehnen <marcus.rehn@hotmail.com>
2025-01-12 21:44:36 +0100

    Adding destructable blocks to slow down enemies

---
+++ obstacle.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+@export var color = "red"
+@export var hp: int = 10
+var current_hp: int = hp
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  pass
+
+
+func hit(attack_power)-> void:
+  current_hp -= attack_power
+  $Sprite.modulate.a = float(current_hp) / float(hp)
+  print_debug("current hp", current_hp)
+  if current_hp <= 0:
+    self.queue_free()
+  
